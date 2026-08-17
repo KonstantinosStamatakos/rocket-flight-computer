65adfd341464d6b311cc80f6510ce6c568f2dd94
Konstantinos Stamatakos <kstam334@gmail.com>
2026-08-17 09:01:40 +0300

    Update bmp581.h

--- firmware/Core/Inc/bmp581.h
+++ firmware/Core/Inc/bmp581.h
@@ -1,29 +1,9 @@
-/*
- * bmp581.h
- *
- *  Created on: Jun 10, 2026
- *      Author: deanstamatakos
- */
-
 #ifndef BMP581_H
 #define BMP581_H
 
 #include "stm32f4xx_hal.h"
 #include <stdint.h>
 
-/*
- * ============================================================
- * bmp581.h
- * ------------------------------------------------------------
- * Minimal BMP581 driver interface.
- *
- * We only expose:
- * - init
- * - read temperature + pressure
- *
- * That is enough for the first flight project stage.
- * ============================================================
- */
 
 typedef struct
 {
